aeb9f52d8733d5c7c78de39c3d020ce0af431c7f
Christopherjh123 <christopherjh@email.com>
2025-05-29 02:29:37 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,13 +10,6 @@ const urlsToCache = [
     '/manifest.json',
     // Add paths to your icons if you create them:
     '/icons/icon-72x72.png',
-    '/icons/icon-96x96.png',
-    '/icons/icon-128x128.png',
-    '/icons/icon-144x144.png',
-    '/icons/icon-152x152.png',
-    '/icons/icon-192x192.png',
-    '/icons/icon-384x384.png',
-    '/icons/icon-512x512.png',
     'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css' // Cache external CSS
 ];
 
